4e65dbab7ca1c5b83c059c04bd904c3b628b2b0a
Sergey Sevostyanov <sermsev@yandex.ru>
2022-10-17 00:22:30 +1000

    Added Task #8

---
+++ HomeWork_1/Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 8: Напишите программу, которая на вход
+принимает положительное целое число (N), а на выходе 
+показывает все чётные числа от 1 до N */
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int index = 2;
+while(index <= n)
+{
+    Console.WriteLine(index);
+    index = index + 2;
+}

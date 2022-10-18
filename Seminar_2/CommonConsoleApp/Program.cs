fa16ba06751fb6a79d8163bb5ac82a11da17b0b3
Sergey Sevostyanov <sermsev@yandex.ru>
2022-10-18 18:49:28 +1000

    Added nasks from Seminar #2

---
+++ Seminar_2/CommonConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введи число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) { max = b; }
+
+if (c > max) { max = c; }
+
+Console.WriteLine($"Максимальное из трех чисел: {max}");
+Console.ReadKey();

db199c77e40e1a057295e8603cd9ebe961a7d266
Sergey Sevostyanov <sermsev@yandex.ru>
2022-10-26 10:27:00 +1000

    seminar 3 files

---
+++ Seminar_3/ConsoleApp9/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 22: Напишите прогу, которая принимает на вход 
+ * число (N) и выдает таблицу квадратов чисел от 1 до N
+ * 5 -> 1,4,9,16,25
+ * 2 -> 1,4
+ * без матриц
+ */
+void PrintSquareToConsole(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i);
+    } 
+}
+
+PrintSquareToConsole(10);

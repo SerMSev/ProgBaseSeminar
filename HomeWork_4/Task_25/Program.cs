f41ce668a1d81003d99fd7a7402fda696c230aa8
Sergey Sevostyanov <sermsev@yandex.ru>
2022-11-08 10:21:12 +1000

    added Task_25

---
+++ HomeWork_4/Task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+Для решения задания использование цикла for является обязательным условием. Не использовать встроенный метод возведения в степень.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите целое число A:");
+int user_value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B:");
+int user_power = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int result = 1;
+
+for (int i = 1; i <= user_power; i++)
+{
+    result = result * user_value;
+}
+
+Console.WriteLine(result);
+
